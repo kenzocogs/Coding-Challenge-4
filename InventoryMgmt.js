ad848e79bc91565f0c88c968f4ea049e4ab9100d
Kenzo Cogswell <kcogswell@usf.edu>
2024-09-13 21:03:49 -0400

    Initialize inventory with product objects

---
+++ InventoryMgmt.js
@@ -0,0 +1,39 @@
+// Task 1: Create an Inventory Array of Product Objects
+let inventory = [
+{
+    name: 'smartPens',
+    price: 70,
+    quantity: 20,
+    lowStockLevel: 5,
+},
+
+{
+    name: 'WirelessCharger',
+    price: 55,
+    quantity: 3,
+    lowStockLevel: 8 ,
+},
+
+{
+    name: 'mechKeyboard',
+    price: 195,
+    quantity: 15,
+    lowStockLevel: 10 ,
+},
+
+{
+    name: 'wallLights',
+    price: 120,
+    quantity: 1,
+    lowStockLevel: 4,
+},
+
+{
+    name: 'graphicsCard',
+    price: 350,
+    quantity: 6,
+    lowStockLevel: 2,
+},
+
+];
+
