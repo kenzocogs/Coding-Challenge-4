bcb97da2f48526343d0a0f21a90f5d19c703f894
Kenzo Cogswell <kcogswell@usf.edu>
2024-09-15 17:06:45 -0400

    Create updateStock function

--- InventoryMgmt.js
+++ InventoryMgmt.js
@@ -41,10 +41,30 @@ let inventory = [
 // Task 2: Create a Function to Display Product Details
 
 function displayProductDetails (item) {
-   let stock = item.quantity <= item.lowStockLevel ? "Low Stock":"In Stock"
-     return `${item.name} is ${stock}`
+   let check = item.quantity <= item.lowStockLevel ? "Low Stock":"In Stock"
+     return `${item.name} is ${check}`
      
 }
 
 console.log(displayProductDetails(inventory[1]))
 // Desired Ouput: WirelessCharger is Low Stock
+
+// Task 3: Create a Function to Update Product Stock After Sales
+
+function updateStock (item, unitsSold) {
+    let afterSales = item.quantity - unitsSold
+
+if (afterSales <= 0)  {
+    return `${item.name} is out of stock`;
+} else if ((0 <= afterSales) && (afterSales <= item.lowStockLevel)) {
+    return `${item.name} is low in stock`;
+} else {
+    return `${item.name} is in stock`;
+}
+
+}
+
+console.log (updateStock(inventory[2], 15))
+// Desired Output: mechKeyboard is out of stock
+
+
